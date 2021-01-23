9a4b70ca55f62149a5bc9c95fe0776c694dfad22
yushph <yushph@gmail.com>
2021-01-23 23:26:07 +0900

    header ok

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,16 +5,16 @@ const Banner = props => (
   <section id="banner" className="major">
     <div className="inner">
       <header className="major">
-        <h1>The Art of Honesty</h1>
-        <h2>
-          正直であろうとするときに<SpanBlock>起こるジレンマ</SpanBlock>
-        </h2>
+        <h1>
+          2021<span style={{ color: 'tomato' }}>.</span> Yoram Mosenzon{' '}
+        </h1>
+        <h2>NVC Workshop in Japan</h2>
       </header>
       <div className="content">
         <p>
           2021 ヨラム・モセンゾン初来日
           <br />
-          沖縄リトリート & 東京2daysワークショップ
+          <SpanBlock>沖縄リトリート & 東京2daysワークショップ</SpanBlock>
         </p>
         <ul className="actions">
           <li>
